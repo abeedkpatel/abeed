2753f4b2e177312602c8f120b4a1c1b829c40df4
abeed patel <abeedkpatel@gmail.com>
2022-10-06 12:36:21 +0300

    this is my third  changes

---
+++ test1.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import numpy as np
+import matplotlib as mlp
+ 
+a = 10
+for i in range(a):
+    print(i)
+if a< 10:
+    print("this code is working")
+else:
+    print("this code is not working")
+
+print("hello world ")
+print("i am excited working on git gub")
+
+print("working in git hub")
